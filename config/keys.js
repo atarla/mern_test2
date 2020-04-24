e6fb94440ff03eebacefdd92f1514309d05e0663
anusha <atarla@uncc.edu>
2020-04-24 13:36:22 -0400

    Remove secret data

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = 'mongodb+srv://anusha:password-1@spa-77jow.mongodb.net/test?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://<username>:<password>1@spa-77jow.mongodb.net/test?retryWrites=true&w=majority';
 
 module.exports = {
-    mongoURI: 'mongodb+srv://anusha:password-1@spa-77jow.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://<username>:<password>@spa-77jow.mongodb.net/test?retryWrites=true&w=majority'
 }
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
